3588fe175f334e749adafb8eb2f85d968118111d
soc-adycoco <adycoco@storans.school.nz>
2026-07-14 21:49:25 +1200

    Started coding ground
    Started coding the scrolling ground
    Player (pigeon) falls off the ground before it repeats
    - Need to test further and fix so that the player reaches the ground before it moves forward

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+#Variables and constants
 var speed
 const START_SPEED = 8.0
+var screensize
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	screensize = get_window().size
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -13,3 +16,6 @@ func _process(_delta: float) -> void:
 	
 	$Player.position.x += speed
 	$Camera.position.x += speed
+	
+	if $Camera.position.x - $Ground.position.x > screensize.x:
+		$Ground.position.x += screensize.x
