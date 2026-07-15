9006341e7dc4119ccfe169506996f1f460496abb
soc-adycoco <adycoco@storans.school.nz>
2026-07-15 22:04:23 +1200

    Edited scrolling ground + added score
    Edited code so ground doesn't move before the player reaches it
    - Ground slightly cuts off the screen/delays before moving forward (will need to improve the ground and background further to avoid the delay before each repeats)
    Created score variable which increases as the game goes on
    Added label node to hud scene to display score

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -4,18 +4,21 @@ extends Node2D
 var speed
 const START_SPEED = 8.0
 var screensize
+var score : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 	screensize = get_window().size
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	speed = START_SPEED
 	
 	$Player.position.x += speed
 	$Camera.position.x += speed
 	
-	if $Camera.position.x - $Ground.position.x > screensize.x:
+	score += speed
+	print(score)
+	
+	if $Camera.position.x - $Ground.position.x > screensize.x * 1.5:
 		$Ground.position.x += screensize.x
