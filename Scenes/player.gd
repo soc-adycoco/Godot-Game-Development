fc0fb15125d290ebc35aa0049dbfb7f323fb8674
soc-adycoco <adycoco@storans.school.nz>
2026-07-20 21:51:16 +1200

    bird_runnerv.12
    Added placeholder sound effects for jumping and ducking, and placeholder background music.
    Changed the Parallax2D scrolling bg to a pixelated one to better fit my game's theme.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -7,15 +7,19 @@ const JUMP_VELOCITY = -1200.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	velocity.y += GRAVITY * delta
+	
+	
 
 	# Handle jump - holding down jump key makes player continuously jump
 	if is_on_floor():
 		#$RunCollision.disabled = false
 		if Input.is_action_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
+			$JumpSFX.play()
 		elif Input.is_action_pressed("ui_down"):
 			$PlayerImage.play("duck")
 			$RunCollision.disabled = true
+			$DuckSFX.play()
 		else:
 			$PlayerImage.play("run")
 	else:
