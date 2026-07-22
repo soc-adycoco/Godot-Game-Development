4478e96eea0d2010f3db633eefe21c666608e366
soc-adycoco <adycoco@storans.school.nz>
2026-07-22 22:11:15 +1200

    bird_runnerv.15
    Added code so user must press spacebar before game starts.
    Added start message to HUD
    Score displays '0' before game starts, and start message disappears when game starts.

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -5,23 +5,37 @@ var speed
 const START_SPEED = 8.0
 var screensize
 var score = 0
+var game_running : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screensize = get_window().size
+	new_game()
 	
+func new_game():
+	score = 0
+	show_score()
+	game_running = false
+	
+	$HUD.get_node("StartLabel").show()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	speed = START_SPEED
-	
-	$Player.position.x += speed
-	$Camera.position.x += speed
-	
-	score += speed
-	show_score()
-	
-	if $Camera.position.x - $Ground.position.x > screensize.x * 1.5:
-		$Ground.position.x += screensize.x
+	if game_running:
+		speed = START_SPEED
+		
+		$Player.position.x += speed
+		$Camera.position.x += speed
+		
+		score += speed
+		show_score()
+		
+		if $Camera.position.x - $Ground.position.x > screensize.x * 1.5:
+			$Ground.position.x += screensize.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
 
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "Score: " + str(score)
