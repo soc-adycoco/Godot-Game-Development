7e8ab818b34c3f54e6e23c92d735f496d3138e85
soc-adycoco <adycoco@storans.school.nz>
2026-08-05 15:08:19 +1200

    bird_runnerv.28 - credit menu + close button
    Connected credit menu to title screen.
    Changed tutorial and credit menu close button texture to change on hover.

--- Scripts/Menus/title_screen.gd
+++ Scripts/Menus/title_screen.gd
@@ -14,6 +14,8 @@ func _process(_delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
-
 func _on_tutorial_button_pressed() -> void:
 	$Tutorial.show()
+
+func _on_credits_button_pressed() -> void:
+	$Credits.show()
