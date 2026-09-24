1464d934d1926959b19b4ca3362eb945a0086977
soc-adycoco <adycoco@storans.school.nz>
2026-09-24 17:13:03 +1200

    bird_runnerv.36 - fixed disappearing ground bug
    Fixed the bug so the ground no longer preemptively shifts and disappears from under the player sprite.
    Now the ground continues staying underneath the player as intended.

--- scripts/level/ground.gd
+++ scripts/level/ground.gd
@@ -4,8 +4,7 @@ var screen_width
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	screen_width = get_window().size
-
+	screen_width = get_viewport_rect().size
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	position.x += screen_width.x
